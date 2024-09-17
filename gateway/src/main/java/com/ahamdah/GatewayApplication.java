f4b1f8baae0f8f320993cb92969d82826407a573
Ahmad Fahad <68047377+AhmdFahad@users.noreply.github.com>
2024-09-17 13:26:22 +0300

    enhance configuration

--- gateway/src/main/java/com/ahamdah/GatewayApplication.java
+++ gateway/src/main/java/com/ahamdah/GatewayApplication.java
@@ -1,5 +1,8 @@
 package com.ahamdah;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,9 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Slf4j
 @Configuration
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApplication implements CommandLineRunner {
 
     @Bean
     @LoadBalanced
@@ -21,4 +25,19 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Value("${eureka.client.enabled}")
+    private String eurekaClientEnabled;
+
+    @Value("${eureka.client.serviceUrl.defaultZone}")
+    private String defaultZone;
+
+    @Override
+    public void run(String... args) throws Exception {
+      log.info("active Profile: {}", profile);
+      log.info("eurekaClientEnabled: {}", eurekaClientEnabled);
+      log.info("defaultZone: {}", defaultZone);
+    }
 }
