ce4f74d79dea2ad609c826c2f25e5c4a6f509df1
Ahmad Fahad <68047377+AhmdFahad@users.noreply.github.com>
2024-09-17 16:21:09 +0300

    configure env

--- resource-service/src/main/java/com/ahamdah/SpringKeycloakauthApplication.java
+++ resource-service/src/main/java/com/ahamdah/SpringKeycloakauthApplication.java
@@ -1,12 +1,23 @@
 package com.ahamdah;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+
 @RestController
 @SpringBootApplication
-public class SpringKeycloakauthApplication {
+public class SpringKeycloakauthApplication implements CommandLineRunner {
+	Logger logger = Logger.getLogger(SpringKeycloakauthApplication.class.getName());
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Hello World");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKeycloakauthApplication.class, args);
